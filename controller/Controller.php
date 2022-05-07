f1222e260a6c34d20e4f92b28ba1798e64de5ca0
Ahmad Nahal <ahmadGhssanNahal@gmail.com>
2022-05-08 00:30:44 +0300

    Allow header

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,6 +1,8 @@
 <?php 
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
 abstract Class Controller{
 
     abstract public function chechRoute($m,$id = null);
